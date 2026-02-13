ffd1082cc703f6ee5cfa53170f1eabbed718fd0e
Gabriel Rosado <86798972+ElHombreRosa@users.noreply.github.com>
2026-02-12 21:33:17 -0500

    add comments , should be completed

--- project3/project3_counselor.c
+++ project3/project3_counselor.c
@@ -1,4 +1,6 @@
 // Name: Gabriel A Rosado Class: COP3514.002S26 
+// Program takes the input of use of the total number of counselors
+// read what counselors have been assigned to a postion and outputs which one hasn't been assigned yet.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,8 +11,8 @@ int search(int counselors[], int n){
     int total_counselors = n * (n + 1) / 2; // gives the sum of n(n+1)/2 if no counselors is missing
     int assigned_counselors = 0;
     
-    for(int i = 0; i < n - 1; i++){ // loops through the array for each assigned counselors (size n - 1)
-        assigned_counselors += counselors[i]; // each counselor present is assigned to assigned_counselors
+    for(int i = 0; i < n - 1; i++){ // loops through the array for each assigned counselors (size n - 1) "assigned all counselors expect one"
+        assigned_counselors += counselors[i]; // each counselor input is assigned to assigned_counselors
     }
     
     return total_counselors - assigned_counselors; // gives the missing counselors number 
