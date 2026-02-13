6d708cefb66523ab3b7a03270e98313cf5508b83
Gabriel Rosado <86798972+ElHombreRosa@users.noreply.github.com>
2026-02-12 20:39:51 -0500

    ---

---
+++ project3/project3_counselor.c
@@ -0,0 +1,40 @@
+// Name: Gabriel A Rosado Class: COP3514.002S26 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int search(int counselors[], int n){
+    
+    int total_counselors = n * (n + 1) / 2; // gives the sum of n(n+1)/2 if no counselors is missing
+    int assigned_counselors = 0;
+    
+    for(int i = 0; i < n - 1; i++){ // loops through the array for each assigned counselors (size n - 1)
+        assigned_counselors += counselors[i]; // each counselor present is assigned to assigned_counselors
+    }
+    
+    return total_counselors - assigned_counselors; // gives the missing counselors number 
+}
+
+
+int main(void){
+    
+    int n;
+    // take the total of counselors
+    printf("Enter number of counselors: ");
+    scanf("%d", &n);
+    
+    int counselors[n-1]; // array to store assigned counselors
+    
+    printf("Enter counselors assigned: ");
+    // read each assigned counselors in array 
+    for(int i = 0; i < n - 1; i++){
+        scanf("%d", &counselors[i]);
+    }
+    
+    int non_assigned = search(counselors, n); // call search function 
+    
+    printf("Counselor has not been assigned: %d", non_assigned);
+    
+    return EXIT_SUCCESS;
+}
